a5b6d5d61775f41991670d0c323fd97680ca77fd
dostonbekqosimov <dostonqosimiy19@gmail.com>
2025-05-10 15:16:08 +0500

    feat: add message repository

--- lesson02/src/messages/messages.controller.ts
+++ lesson02/src/messages/messages.controller.ts
@@ -1,22 +1,24 @@
 import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesRepository } from './messages.repository';
 
 @Controller('messages')
 export class MessagesController {
-  constructor(private readonly messagesService: MessagesService) {
-  }
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly messageRepo: MessagesRepository,
+  ) {}
 
   @Get()
-  listMessages() {
-  }
+  listMessages() {}
 
   @Post()
   addMessages(@Body() request: CreateMessageDto) {
     console.log(request);
+    return this.messageRepo.findOne('12');
   }
 
   @Get('/:id')
-  getMessageById(@Param('id') messageId: string) {
-  }
+  getMessageById(@Param('id') messageId: string) {}
 }
